3062214251dd6fee044e150aa9102ec8f71f891c
Quentin_Droucheau <quentin.droucheau@etu.univ-orleans.fr>
2023-01-18 11:22:32 +0100

    update

--- client_joueur_quentin.py
+++ client_joueur_quentin.py
@@ -9,6 +9,8 @@ import joueur
 
 from math import *
 
+ETAT = 0
+
 def mon_IA(ma_couleur,carac_jeu, plan, les_joueurs):
     """ Cette fonction permet de calculer les deux actions du joueur de couleur ma_couleur
         en fonction de l'état du jeu décrit par les paramètres. 
@@ -29,24 +31,43 @@ def mon_IA(ma_couleur,carac_jeu, plan, les_joueurs):
         str: une chaine de deux caractères en majuscules indiquant la direction de peinture
             et la direction de déplacement
     """
-    pos = None
-    joueurs = les_joueurs.split("\n")
-    for joueur in joueurs:
-        info = joueur.split(";")
-        if info[0] == ma_couleur:
-            pos = (int(info[5]), int(info[6]))
-
     plateau_jeux = plateau.Plateau(plan)
+
+    joueurs = joueurs_info(les_joueurs)
+    objet = objets(plateau_jeux)
+
+    direction = random.choice("NSEO")
+    if danger(plateau_jeux, (joueurs[ma_couleur][4], joueurs[ma_couleur][5])):
+        direction = deplace_danger(plateau_jeux, (joueurs[ma_couleur][4], joueurs[ma_couleur][5]))
+    elif len(objet) > 0:
+        direction = direction_chemin(plateau_jeux, (joueurs[ma_couleur][4], joueurs[ma_couleur][5]), (objet[0][0], objet[0][1]))
+
+    peindre = random.choice("XNSOE")
+
+    return peindre+""+direction
+
+def recup_objet(plateau_jeux, joueurs, objet1, objet2):
+    distance_joueur = dict()
+    for joueur, info in joueurs.items():
+        #distance_joueur[joueur] = distance(plateau_jeux, (objet))
+        pass
+
+def joueurs_info(les_joueurs):
+    positions = dict()
+    for joueur in les_joueurs.split("\n"):
+        j = joueur.split(";")
+        positions[j[0]] = (int(j[1]), int(j[2]), int(j[3]), int(j[4]), int(j[5]), int(j[6]))
+    return positions
+
+def objets(plateau_jeux):
+    objets_pl = []
     for l in range(plateau.get_nb_lignes(plateau_jeux)):
         for c in range(plateau.get_nb_colonnes(plateau_jeux)):
             casepl = plateau.get_case(plateau_jeux, (l, c))
-            if case.get_objet(casepl) != const.AUCUN:
-                if pos is None:
-                    return "XN"
-                else:
-                    return "X"+direction_chemin(plateau_jeux, pos, (l, c))
-
-    return random.choice("XNSOE")+random.choice("NSEO")
+            objet = case.get_objet(casepl)
+            if objet != const.AUCUN:
+                objets_pl.append((l, c, objet))
+    return objets_pl
 
 def calque(plateau_jeux, pos1):
     liste = dict()
@@ -65,6 +86,19 @@ def calque(plateau_jeux, pos1):
             positions.remove(pos)
     return liste
 
+def distance(plateau_jeux, pos1, pos2):
+    liste = []
+    liste.append(pos1)
+    calque_plateau = calque(plateau_jeux, pos2)
+    while pos1 in calque_plateau.keys() and calque_plateau[pos1] != 0:
+        for d, p in plateau.INC_DIRECTION.items():
+            new_pos = (pos1[0] + p[0], pos1[1] + p[1])
+            if new_pos in calque_plateau.keys():
+                if calque_plateau[new_pos] + 1 == calque_plateau[pos1]:
+                    pos1 = new_pos
+                    liste.append(new_pos)
+    return len(liste)
+
 def direction_chemin(plateau_jeux, pos1, pos2):
     calque_plateau = calque(plateau_jeux, pos2)
     while pos1 in calque_plateau.keys() and calque_plateau[pos1] != 0:
@@ -73,8 +107,7 @@ def direction_chemin(plateau_jeux, pos1, pos2):
             if new_pos in calque_plateau.keys():
                 if calque_plateau[new_pos] + 1 == calque_plateau[pos1]:
                     pos1 = new_pos
-                    return d
-    return "N"
+    return random.choice("NSEO")
 
 with open("plans/plan2.txt") as fic:
     plan=fic.read()
@@ -83,7 +116,7 @@ with open("plans/plan2.txt") as fic:
 #c = calque(p, (5, 11))
 #print(chemin(p, (0, 10), (13, 1)))
 
-def danger(plateau_jeux, pos, distance_max):
+def danger(plateau_jeux, pos, distance_max=5):
     nb = 0
     for direction in plateau.INC_DIRECTION:
         nb += plateau.nb_joueurs_direction(plateau_jeux, pos, direction, distance_max)
@@ -91,16 +124,15 @@ def danger(plateau_jeux, pos, distance_max):
         return True
     return False
 
-def deplace_danger(plateau_jeux, pos, distance_max):
+def deplace_danger(plateau_jeux, pos, distance_max=5):
     directions = plateau.directions_possibles(plateau_jeux, pos)
     nb_min = 0
     direction = None
-    for d in directions.keys:
-        nb = plateau.nb_joueurs_direction(plateau_jeux, pos, direction, distance_max)
+    for d in directions.keys():
+        nb = plateau.nb_joueurs_direction(plateau_jeux, pos, d, distance_max)
         if direction is None or nb_min < nb:
             direction = d
             nb_min = nb
-        
     return direction
 
 
